62b0667236a7a57e1066b04f7bc677ac1c4bd1d2
MartinSilvaJose <118887247+MartinSilvaJose@users.noreply.github.com>
2024-04-03 20:27:02 +0200

    Update UseMovies.js

--- projects/05-react-buscador-peliculas/src/Hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/Hooks/useMovies.js
@@ -6,6 +6,7 @@ export function useMovies({ search, sort }) {
   const [loading, setLoading] = useState(false)
   // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState(null)
+
   const previusSearch = useRef(search)
 
   const getMovies = useCallback(async ({ search }) => {
